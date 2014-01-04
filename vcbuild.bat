ac381790b44454c689977841901e025047243f22
Julian Haupt <jh@spielmeister.com>
2014-01-04 19:54:28 +0100

    changed vcbuild.bat script back to work with vs2010 & vs2012. vs2013 is not supported

--- vcbuild.bat
+++ vcbuild.bat
@@ -1,6 +1,10 @@
 @echo off
 
-call C:\"Program Files (x86)"\"Microsoft Visual Studio 12.0"\VC\vcvarsall.bat x86
+IF exist  C:\"Program Files (x86)"\"Microsoft Visual Studio 11.0"\nul ( 
+call C:\"Program Files (x86)"\"Microsoft Visual Studio 11.0"\VC\vcvarsall.bat x86
+) ELSE ( 
+call C:\"Program Files (x86)"\"Microsoft Visual Studio 10.0"\VC\vcvarsall.bat x86
+)
 
 cd %~dp0
 
